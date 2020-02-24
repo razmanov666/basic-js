8b73ab7eafdabc8836ffc0d47b8b3eb73d1c2b22
Alexey Razmanov <razmanov666@gmail.com>
2020-02-24 15:50:08 +0300

    feat: complite tasks count-cats & dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,17 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+function countCats(arr){
+  let count=0;
+  arr.forEach( line => {
+    typeof(element) != 'string' ? doNothing() : 
+    line.forEach (element => {element == "^^" ? count++ : count;
+    //console.log(element);
+  })
+  });
+  return count;
+}
+
+function doNothing(){
+
+}
+
+let arr=['aa', '##', false, NaN, 2, 3, '^ ^', undefined, 54, ' ^^'];
+console.log(countCats(arr))
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,39 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+function createDreamTeam(arr) {
+  let dream=[];
+  arr.forEach(element => {typeof(element) != 'string' ? arr.indexOf(element, [0]) : doPush(dream, element)});
+  return dream.sort().join('');
 };
+
+function doNothing(){
+
+}
+
+function doPush(dream,element){
+  let i=0;
+  element = element.split('');
+  let firstSymbol;
+  for (let n=0; n<=element.length; n++){
+    if (element[n] == " ")
+    { i++;}
+    else 
+    break;}
+    firstSymbol = element[i].toUpperCase()
+      return isNaN(parseInt(firstSymbol)) ? dream.push(firstSymbol) : doNothing() ; 
+}
+
+let team = [
+  ['David Abram'],
+  ['Robin Attfield'],
+  'Thomas Berry',
+  ['Paul R.Ehrlich'],
+  'donna Haraway',
+  ' BrIaN_gOodWiN  ',
+  {
+    0: 'Serenella Iovino'
+  },
+  'Erazim Kohak',
+  '  val_plumwood',
+];
+                         
+let GLRD = ['GLRD'];
+console.log(createDreamTeam(team));
